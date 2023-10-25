33db95394e2da825919523e8fc013a82b3b88a6b
giovannimegliola <giovanni.megliola@hotmail.it>
2023-10-25 16:38:08 +0200

    generated random num for puntiFatti e falliSubiti

--- snack_4/js/script.js
+++ snack_4/js/script.js
@@ -3,7 +3,7 @@
 // PROBLEMA: Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti. Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
 
-//array con oggetti le attuali prime 10 squadre della serie A 
+//array con oggetti le (attuali al 25/10) prime 10 squadre della serie A 
 
 const serieA = [
   {
@@ -60,6 +60,12 @@ const serieA = [
 ]        
 console.log(serieA);
 
+
+
+serieA.forEach((team) => {
+  team.puntiFatti = Math.floor(Math.random() * 114) + 1;
+  team.falliSubiti = Math.floor(Math.random() * 200) + 1;
+});
 
 
 
